3001c496ac2884bc924594e7838bdb30ab633a79
David Etkin <davidetkin@davidetkin.com>
2020-08-27 20:14:05 -0400

    set new colors and tabbed categories page

--- app/controllers/affirmations_controller.rb
+++ app/controllers/affirmations_controller.rb
@@ -1,5 +1,6 @@
 class AffirmationsController < ApplicationController
   def index
-    @affirmations = Affirmation.all
+    @affirmations = Affirmation.all.order("id ASC")
+    @categories = Category.all.order("id ASC")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_010823) do
+ActiveRecord::Schema.define(version: 2020_08_27_231835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2020_08_21_010823) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
