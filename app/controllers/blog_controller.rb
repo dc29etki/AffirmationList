6853df3ce34b96cb9f67ee4e27c1b36925ab79b1
David Etkin <davidetkin@davidetkin.com>
2020-09-02 12:20:53 -0400

    html safe blogs

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,7 @@
 class BlogController < ApplicationController
   def index
-    @blogs = Blog.order("id ASC");
+    @pinned = Blog.where(pinned: true).order("id ASC");
+    @blogs = Blog.where(pinned: false).order("id ASC");
   end
   def viewblog
     @blog = Blog.find(params[:id])
