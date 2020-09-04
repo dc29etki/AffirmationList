9aef6330494b6b99dd9d302485f9277c0c30c60d
David Etkin <davidetkin@davidetkin.com>
2020-09-04 10:54:59 -0400

    style mostly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_004523) do
+ActiveRecord::Schema.define(version: 2020_09_04_144107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_004523) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "pinned"
+    t.boolean "pinned", default: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -44,7 +44,9 @@ ActiveRecord::Schema.define(version: 2020_09_02_004523) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin?"
+    t.boolean "admin?", default: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
