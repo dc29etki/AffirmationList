c8603443e8c5af1f74830c751f9d5caf98d0ee24
David Etkin <davidetkin@davidetkin.com>
2020-09-04 14:05:36 -0400

    removed prints

--- app/controllers/view_affirmations_controller.rb
+++ app/controllers/view_affirmations_controller.rb
@@ -6,10 +6,8 @@ class ViewAffirmationsController < ApplicationController
     sub = params[:sub]
     sub[0] = sub[0].capitalize
     
-    p sub
     
     if category.casecmp('success')
-      p category
       @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', sub, sub, sub, sub, sub)
     end
     
