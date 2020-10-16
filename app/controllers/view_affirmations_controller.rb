3d857bfa533a06a8a72087f1008d4732097a8f59
David Etkin <davidetkin@davidetkin.com>
2020-10-16 13:56:35 -0400

    final changes

--- app/controllers/view_affirmations_controller.rb
+++ app/controllers/view_affirmations_controller.rb
@@ -5,33 +5,35 @@ class ViewAffirmationsController < ApplicationController
     @category = params[:category]
     @sub = params[:sub]
     @sub[0] = @sub[0].capitalize
-    
-    
-    if @category == 'success'
+
+    if @category == 'coronavirus'
+      @cat = Category.find(10)
+      @affirmations = Success.where('tags=?', @sub) + LoveAndRelationship.where('tags=?', @sub) + SelfFulfillment.where('tags=?', @sub) + Lifestyle.where('tags=?', @sub) + PhysicalHealth.where('tags=?', @sub) + MentalHealth.where('tags=?', @sub) + Spirituality.where('tags=?', @sub) + SafeAndSecure.where('tags=?', @sub)
+elsif @category == 'success'
       puts @category
       @cat = Category.find(1)
-      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     elsif @category == 'love_relationships'
       @cat = Category.find(2)
-      @affirmations = LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     elsif @category == 'self_fulfillment'
       @cat = Category.find(3)
-      @affirmations = SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     elsif @category == 'lifestyle'
       @cat = Category.find(4)
-      @affirmations = Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     elsif @category == 'physical_health'
       @cat = Category.find(5)
-      @affirmations = PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     elsif @category == 'mental_health'
       @cat = Category.find(6)
-      @affirmations = MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     elsif @category == 'spirituality'
       @cat = Category.find(7)
-      @affirmations = Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     elsif @category == 'safe_secure'
       @cat = Category.find(8)
-      @affirmations = SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
+      @affirmations = Success.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + LoveAndRelationship.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SelfFulfillment.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Lifestyle.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + PhysicalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + MentalHealth.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + Spirituality.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub) + SafeAndSecure.where('sub1=? OR sub2=? OR sub3=? OR sub4=? OR sub5=?', @sub, @sub, @sub, @sub, @sub)
     end
     
   end
