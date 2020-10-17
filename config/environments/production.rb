14c0e6607ac9a316d0b1107d075a4a27151d88f0
David Etkin <davidetkin@davidetkin.com>
2020-10-17 12:31:52 -0400

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'affirmationlist.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :user_name            => 'affirmationlist@gmail.com',
+        :password             => 'domji7-gapdog-Gybzim',
+        :authentication       => 'plain',
+        :enable_starttls_auto => true  }
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
