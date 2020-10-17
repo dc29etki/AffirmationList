415629afbcb56d155e51153c2842836d296da52a
David Etkin <davidetkin@davidetkin.com>
2020-10-17 12:42:19 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "uppermation_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'affirmationlist.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.affirmationlist.com' }
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
         :address              => "smtp.gmail.com",
